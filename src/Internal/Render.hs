a99b08e5dcfa4b1e89437a508afa4b60f20def29
Yun-Yan Chi <jaiyalas@gmail.com>
2016-01-17 23:55:59 +0800

    modularized

---
+++ src/Internal/Render.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Internal.Render where
+--
+import qualified SDL
+--
+import Data.Word (Word8(..))
+import Data.Text (Text(..),pack,append)
+import Data.Monoid (Last(..))
+import Data.Foldable (foldMap)
+import Data.Maybe (maybe,catMaybes)
+import Linear.Affine (Point(..))
+import Linear.V2 (V2(..))
+import Linear.V4 (V4(..))
+--
+import Internal.Types
+--
+drawAuxLines :: SDL.Renderer
+             -> WH -- ^ window W/H
+             -> Pt -- ^ mouse X/Y
+             -> IO ()
+drawAuxLines rdr (V2 winW winH) (P (V2 mouseX mouseY)) = do
+   SDL.rendererDrawColor rdr SDL.$=
+      V4 minBound minBound minBound maxBound
+   SDL.drawLine rdr (P$V2 0 mouseY) (P$V2 winW mouseY)
+   --
+   SDL.rendererDrawColor rdr SDL.$=
+      V4 maxBound maxBound maxBound maxBound
+   SDL.drawLine rdr (P$V2 mouseX 0) (P$V2 mouseX winH)
